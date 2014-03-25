98fd432d85c6860d92f71e08217fe1db8c654a53
Lucas Waye <wayetender@gmail.com>
2014-03-25 10:52:41 -0400

    script update, oops!

--- runworker.sh
+++ runworker.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+if [ $# -lt 1 ]; then
+    echo "usage: runclient.sh [queueHostname]"
+    exit
+fi
+
 java -cp group4.jar \
      -Djava.rmi.server.codebase="`cat rmicodebases`" \
      -Djava.rmi.server.hostname=`cat myhostname` \
      -Djava.security.policy=security.policy \
      -Djava.rmi.server.useCodebaseOnly=false \
-        edu.harvard.cs262.ComputeServer.Group4.WorkerServer `cat myhostname`
+        edu.harvard.cs262.ComputeServer.Group4.WorkerServer ${1}
 
